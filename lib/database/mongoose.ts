22cde2b4b3c5eccdd49e79eced955e5260287d99
Girish@gaming_pavilion <Girish@gaming_pavilion>
2024-03-22 19:58:57 -0400

    mongoose conn implementation

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
+      dbName: 'Unveiledpixel', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
